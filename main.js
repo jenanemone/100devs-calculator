1871d48c1d366d956ea547096a7ab96de12fb393
Jennie Allen <jenanemone@gmail.com>
2022-04-25 01:26:52 -0500

    Still buggy, but decimals work

--- main.js
+++ main.js
@@ -12,120 +12,175 @@ into the first term of the next operation, followed by the existing operator (th
 // and the methods that are available for the manipulation and display of the work performed.
 
 class Calculator {
-    
+
     // constructor requires a string phrase, numerical evaluation, and hasOperator flag
     constructor() {
         this.displayPhrase = '';
+        this.phraseArray;
         this.evaluated = 0;
-        this.hasOperator = false; // this is to mimic the behavior of old
+        this.hasOperator = false; // this is to mimic the behavior of old calculators
+        this.hasDecimalOperand1 = false;
+        this.hasDecimalOperand2 = false;
+        this.hasPower = false; // off to start
     }
 
     // term receives the numerical data only and adds those digits (or .) to the displayPhrase
     // TODO: limit entry of "." button register to one per term.
     term(element) {
-        let enteredElement = element.target;
-        let val = enteredElement.innerText;
-        
-        // remove the leading 0 to make it purty
-        // TODO: leave leading 0 if first button registered is '.'
-        if (this.displayPhrase == '0') {
-            this.displayPhrase = '';
+        if (this.hasPower) {
+            let enteredElement = element.target;
+            let val = enteredElement.innerText;
+
+            // remove the leading 0 to make it purty
+            // TODO: leave leading 0 if first button registered of 2nd operand is "."
+            // TODO: debug!
+
+            if (this.hasDecimalOperand1 && this.hasDecimalOperand2 && val == '.') {
+                return;
+            }
+            else if (!this.hasOperator && this.hasDecimalOperand1 && val == '.') {
+                return;
+            }
+            else if (this.hasOperator && this.hasDecimalOperand2 && val == '.') {
+                return;
+            }
+            else if (this.displayPhrase == '0' && val == '.') {
+                this.displayPhrase = '0.';
+                this.hasDecimalOperand1 = true;
+            }
+            else if (this.hasOperator && !this.hasDecimalOperand2 && val == '.') {
+                this.displayPhrase = this.displayPhrase + "0.";
+                this.hasDecimalOperand2 = true;
+            }
+            else if (val != '.') {
+                this.displayPhrase = this.displayPhrase + val;
+            }
+
+            // if (this.hasDecimalOperand1 && val == '.' || this.hasDecimalOperand2 && val == '.') {
+            //     return;
+            // }
+            // else if (this.displayPhrase.length == 0 && val == '.') {
+            //     this.displayPhrase = "0";
+            // }
+            // else if (val == '.') {
+            //     if (this.hasOperator) {
+            //         this.hasDecimalOperand2 = true;
+            //     }
+            //     else {
+            //         this.hasDecimalOperand1 = true;
+            //     }
+            //     this.displayPhrase = this.displayPhrase + val;
+            // }
+            // else {
+            //     this.displayPhrase = this.displayPhrase + val;
+            // }
+
+            document.querySelector('.viewWindow').innerText = this.displayPhrase;            
         }
-        
-        this.displayPhrase = this.displayPhrase + val;
-
-        document.querySelector('.viewWindow').innerText = this.displayPhrase;
-
         return this;
     }
 
     // operation receives the operator's button register. 
     // One operation registered will allow entry of second term.
     // Two operations registered and two terms registered triggers this.calculate() --
-        // and stores that evaluation as the first term, along with the operator
+    // and stores that evaluation as the first term, along with the operator
     // TODO: verify behavior of term / 0 
     // TODO: allow for changing expression when only one term exists
+    // TODO: fix behavior after evaluation.
     operation(element) {
 
-        // grab the operator from the registered button and append to string
-        let enteredElement = element.target;
-        let val = enteredElement.innerText;
-        this.displayPhrase += ` ${val} `;
+        if (this.hasPower) {
 
-        // Split the string to separate terms
-        let phraseArray = this.displayPhrase.split(' '); // test if operator is legal
-        
-        console.log(phraseArray);
-        
-        // If an operator and two terms exist, trigger this.calculate(),
-        // otherwise set hasOperator to true
-        if (this.hasOperator && phraseArray.length > 3) {
-            
-            console.log('calling calculate');
-            this.calculate(); 
-            console.log(this.evaluated);
-            this.displayPhrase = this.evaluated;
-            
-        }
-        else {
-            this.hasOperator = true;
-        }
-        
-        console.log(this.displayPhrase);
+            // grab the operator from the registered button and append to string
+            let enteredElement = element.target;
+            let val = enteredElement.innerText;
+            this.displayPhrase += ` ${val} `;
 
-        document.querySelector('.viewWindow').innerText = this.displayPhrase;
-        
-        return this;
+            // Split the string to separate terms
+            let terms = this.displayPhrase.split(' '); // test if operator is legal
+
+            console.log(terms);
+
+            // If an operator and two terms exist, trigger this.calculate(),
+            // otherwise set hasOperator to true
+            if (this.hasOperator && terms.length > 3) {
+
+                console.log('calling calculate');
+                this.calculate();
+                console.log(this.evaluated);
+                this.displayPhrase = this.evaluated;
+
+            }
+            else {
+                this.hasOperator = true;
+            }
+
+            console.log(this.displayPhrase);
+
+            document.querySelector('.viewWindow').innerText = this.displayPhrase;
+
+            return this;
+        }
     }
 
     // calculate() is called either from equals button register or 
     // from having two terms and two operators
     // TODO: debug and check behaviors
     calculate() {
-        
-        let expr = this.displayPhrase.split(' ');
-        let operand1 = +expr[0];
-        let op = expr[1];
-        let operand2 = +expr[2];
+        if (this.hasPower) {
 
-        console.log('lets go switch',operand1,op,operand2);
+            let expr = this.displayPhrase.split(' ');
+            let operand1 = +expr[0];
+            let op = expr[1];
+            let operand2 = +expr[2];
 
-        // console.log(expr);
-        switch (op) {
-            case '+' : 
-                this.evaluated = operand1 + operand2;
-                break;
-            case '-' :
-                this.evaluated = operand1 - operand2;
-                break;
-            case '/' :
-                this.evaluated = operand1 / operand2;
-                break;
-            case '*' :
-                this.evaluated = operand1 * operand2;
-                break;
-            default :
-                console.log('Oops')
-                break;
+            console.log('lets go switch', operand1, op, operand2);
+
+            // console.log(expr);
+            switch (op) {
+                case '+':
+                    this.evaluated = operand1 + operand2;
+                    break;
+                case '-':
+                    this.evaluated = operand1 - operand2;
+                    break;
+                case '/':
+                    this.evaluated = operand1 / operand2;
+                    break;
+                case '*':
+                    this.evaluated = operand1 * operand2;
+                    break;
+                default:
+                    this.evaluated = operand1;
+            }
+            console.log(this.evaluated);
+
+            if (this.evaluated % 1 == 0) {
+                this.hasDecimalOperand1 = false;
+
+            }
+
+            this.hasOperator = false;
+            this.hasDecimalOperand2 = false;
+            this.displayPhrase = this.evaluated.toString();
+            document.querySelector('.viewWindow').innerText = this.displayPhrase;
         }
-        console.log(this.evaluated);
-
-        document.querySelector('.viewWindow').innerText = this.displayPhrase;
-
-        this.hasOperator = false;
-        this.displayPhrase = this.evaluated.toString();
         return this;
     }
 
     // allClear() deletes calc data and returns hasOperator to flase
     allClear() {
 
-        this.displayPhrase = '0';
-        document.querySelector('.viewWindow').innerText = '0';
+        if (this.hasPower) {
 
-        this.hasOperator = false;
-        this.evaluated = 0;
+            this.displayPhrase = '';
+            document.querySelector('.viewWindow').innerText = '0';
 
+            this.hasOperator = false;
+            this.hasDecimalOperand1 = false;
+            this.hasDecimalOperand2 = false;
+            this.evaluated = 0;
+        } 
         return this;
     }
 
@@ -134,6 +189,9 @@ class Calculator {
 
         document.querySelector('.viewWindow').innerText = '';
         this.hasOperator = false;
+        this.hasPower = false;
+        this.hasDecimalOperand1 = false;
+        this.hasDecimalOperand2 = false;
         this.evaluated = 0;
 
         this.displayPhrase = '';
@@ -141,37 +199,43 @@ class Calculator {
         return this;
     }
 
-    // TODO: test functionality of on() so that it sets displayPhrase to 0 and adds to viewWindow
+    // on() turns hasPower flag to true and clears any data
     on() {
-
+        this.hasPower = true;
+        this.displayPhrase = '0';
         document.querySelector('.viewWindow').innerText = '0';
         this.hasOperator = false;
+        this.hasDecimalOperand1 = false;
+        this.hasDecimalOperand2 = false;
+        this.phraseArray = [];
         this.evaluated = 0;
-        this.displayPhrase = '';
+  
         return this;
     }
 
     // clear() removes the last term and operator 
     // TODO: debug
     clear() {
+        if (this.hasPower) {
 
-        let terms = this.displayPhrase.split(' ');
-        // if there is only one term, clear will remove the one term and operator
-        // if there are multiple terms, clear will remove the second term and the operator
-        if (this.hasOperator) {
-            this.displayPhrase = terms[0];
-            console.log(this.displayPhrase);
-            this.hasOperator = false;
+            let terms = this.displayPhrase.split(' ');
+            // if there is only one term, clear will remove the one term and operator
+            // if there are multiple terms, clear will remove the second term and the operator
+            if (this.hasOperator) {
+                this.displayPhrase = terms[0];
+                console.log(`inside hasOperator: ${this.displayPhrase}`);
+                document.querySelector('.viewWindow').innerText = this.displayPhrase;
+                this.hasOperator = false;
+            }
+            else {
+                this.displayPhrase = '';
+                console.log(`inside else (no hasOperator) ${this.displayPhrase}`);
+                document.querySelector('.viewWindow').innerText = '0';
+                this.hasDecimalOperand1 = false;
+                this.hasDecimalOperand2 = false;
+                this.hasOperator = false;
+            }
         }
-        else {
-            this.displayPhrase = '';
-            console.log(this.displayPhrase);
-        }
-
-        this.displayPhrase = terms.join(' ');
-
-        document.querySelector('.viewWindow').innerText = this.displayPhrase;
-        
         return this;
     }
 }
@@ -181,14 +245,14 @@ let calc = new Calculator();
 
 // Let's go with some event listeners...
 let numsDOM = document.querySelectorAll('.num');
-numsDOM.forEach(element => element.addEventListener('click',(element) => calc.term(element)));
+numsDOM.forEach(element => element.addEventListener('click', (element) => calc.term(element)));
 let operatorsDOM = document.querySelectorAll('.operator');
-operatorsDOM.forEach(element => element.addEventListener('click',(element) => calc.operation(element)));
-document.querySelector('.equals').addEventListener('click',() => calc.calculate());
-document.querySelector('.AC').addEventListener('click',() => calc.allClear());
-document.querySelector('.off').addEventListener('click',() => calc.off())
-document.querySelector('.on').addEventListener('click',() => calc.allClear());
-document.querySelector('.clear').addEventListener('click',() => calc.clear());
+operatorsDOM.forEach(element => element.addEventListener('click', (element) => calc.operation(element)));
+document.querySelector('.equals').addEventListener('click', () => calc.calculate());
+document.querySelector('.AC').addEventListener('click', () => calc.allClear());
+document.querySelector('.off').addEventListener('click', () => calc.off())
+document.querySelector('.on').addEventListener('click', () => calc.on());
+document.querySelector('.clear').addEventListener('click', () => calc.clear());
 
 
 // TODO: Add multi-mode toggle and methods to allow for the evaluation of multiple terms and operators.
